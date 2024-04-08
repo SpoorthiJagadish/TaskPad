2f8cb92dbb425f93b88916583ca9263ec3b27abd
SpoorthiJagadish <spoorthijagadish03@gmail.com>
2024-04-08 16:00:08 +0530

    implemented mongdb connection

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectToMongoDB from "./dbConn/connectToMongoDB.js";
 
 const app = express();
+dotenv.config();
+
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   // root route http://localhost:5000/
   res.send("Hello World");
 });
 
-app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server Running on port ${PORT}`)
+});
