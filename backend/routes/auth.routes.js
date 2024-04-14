02b8419dba2f7488cbfb7b62ea2c9f28a9701b2e
tanush27 <tanush.jadhav.btech2021@sitpune.edu.in>
2024-04-14 10:27:24 +0530

    Solved Errors in commit - Implemented Authentication

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,13 +1,11 @@
-  import express from "express";
-  import {signup,login, logout} from "../controllers/auth.controller.js"
+import express from "express";
+import { signup, login, logout } from "../controller/auth.controller.js";
 
-  const router = express.Router();
+const router = express.Router();
 
-  router.post("/api/auth/signup",signup);
+router.post("/api/auth/signup", signup);
 
+router.post("/api/auth/login", login);
 
-  router.posg("/api/auth/login",login);
-
-
-  router.post("/api/auth/logout",logout);
-  export default router;
+router.post("/api/auth/logout", logout);
+export default router;
